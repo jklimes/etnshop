3266bf6f7b4463f08a29195c496f1933adbe9345
Jan Klimes <jenda.klimes@centrum.cz>
2016-06-27 02:41:03 +0200

    basic tests of ProductServiceImpl: used separate test db in test app context with 'test' spring profile moved app context from WEB-INF to resources to make testing using classpath easy added required lib dependencies in gradle fixed delete method in ProductDao - changed native SQL to HQL

--- etnshop/src/main/java/cz/etn/etnshop/dao/ProductDaoImpl.java
+++ etnshop/src/main/java/cz/etn/etnshop/dao/ProductDaoImpl.java
@@ -1,39 +1,37 @@
 package cz.etn.etnshop.dao;
 
-import java.util.List;
-
-import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("productDao")
 public class ProductDaoImpl extends AbstractDao implements ProductDao {
 
-	@Override
-	public void saveProduct(Product product) {
-		persist(product);
-	}
+    @Override
+    public void saveProduct(Product product) {
+        persist(product);
+    }
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<Product> getProducts() {
-		 Criteria criteria = getSession().createCriteria(Product.class);
-	     return (List<Product>) criteria.list();
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Product> getProducts() {
+        Query query = getSession().createQuery("select p from Product p order by p.id");
+        return (List<Product>) query.list();
+    }
 
-	@Override
-	public void deleteProduct(int productId) {
-		Query query = getSession().createSQLQuery("delete from Product where id = :id");
+    @Override
+    public void deleteProduct(int productId) {
+        Query query = getSession().createQuery("delete from Product where id = :id");
         query.setInteger("id", productId);
         query.executeUpdate();
-	}
+    }
 
 
-	@Override
-	public void updateProduct(Product product) {
-		getSession().update(product);
-		
-	}
-	
+    @Override
+    public void updateProduct(Product product) {
+        getSession().update(product);
+    }
+
 
 }
---
+++ etnshop/src/test/java/cz/etn/etnshop/service/ProductServiceImplTest.java
@@ -0,0 +1,128 @@
+package cz.etn.etnshop.service;
+
+import cz.etn.etnshop.dao.Product;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+@ActiveProfiles("test")
+@ContextConfiguration(locations = {"classpath:appContext/spring-core-config.xml", "classpath:spring-core-config_test.xml"})
+public class ProductServiceImplTest {
+    @Resource(name = "productService")
+    private ProductService productService;
+    @Resource
+    private SessionFactory sessionFactory;
+
+    private Product createProduct(String name, String serialNumber) {
+        Product p = new Product();
+        p.setName(name);
+        p.setSerialNumber(serialNumber);
+        return p;
+    }
+
+    private void flushAndClear() {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.flush();
+        currentSession.clear();
+    }
+
+    private List<Product> createAndSaveProducts(int numberOfProducts) {
+        List<Product> products = createProducts(numberOfProducts);
+        for (Product product : products) {
+            productService.saveProduct(product);
+        }
+        return products;
+    }
+
+    private List<Product> createProducts(int count) {
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            products.add(createProduct("testSave" + i, "0" + i));
+        }
+        return products;
+    }
+
+
+    @Test
+    public void saveProduct() throws Exception {
+        Product product = createProduct("testSave", "00000000001");
+        productService.saveProduct(product);
+
+        flushAndClear();
+
+        assertTrue("Product id should greater than zero.", product.getId() > 0);
+        Session session = sessionFactory.getCurrentSession();
+        assertFalse(session.contains(product));
+
+        Product loadedProduct = (Product) session.load(Product.class, product.getId());
+        assertProductByProduct(product, loadedProduct);
+    }
+
+    private void assertProductByProduct(Product product, Product loadedProduct) {
+        assertEquals(product.getId(), loadedProduct.getId());
+        assertEquals(product.getName(), loadedProduct.getName());
+        assertEquals(product.getSerialNumber(), loadedProduct.getSerialNumber());
+    }
+
+    @Test
+    public void getProducts() throws Exception {
+        final int numberOfProducts = 10;
+        List<Product> products = createAndSaveProducts(numberOfProducts);
+
+        flushAndClear();
+
+        List<Product> loadedProducts = productService.getProducts();
+        assertEquals(numberOfProducts, loadedProducts.size());
+        for (int i = 0; i < numberOfProducts; i++) {
+            assertProductByProduct(products.get(i), loadedProducts.get(i));
+        }
+    }
+
+
+    @Test
+    public void deleteProduct() throws Exception {
+        final int numberOfProducts = 10;
+        List<Product> products = createAndSaveProducts(numberOfProducts);
+
+        flushAndClear();
+
+        int idToDelete = products.get(0).getId();
+        productService.deleteProduct(idToDelete);
+        List<Product> loadedProducts = productService.getProducts();
+        assertEquals(numberOfProducts - 1, loadedProducts.size());
+        assertFalse("loaded products must not contain product with deleted id", loadedProducts.stream()
+                .anyMatch(product -> product.getId() == idToDelete));
+
+    }
+
+    @Test
+    public void updateProduct() throws Exception {
+        List<Product> products = createAndSaveProducts(10);
+
+        flushAndClear();
+
+        Product product = products.get(0);
+        product.setName("updatedName");
+        product.setSerialNumber("99999999999");
+        productService.updateProduct(product);
+
+        flushAndClear();
+
+        List<Product> loadedProducts = productService.getProducts();
+        assertProductByProduct(product, loadedProducts.get(0));
+    }
+
+}
